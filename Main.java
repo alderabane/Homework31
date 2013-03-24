cd81fbf0002efbbaf44f0e55d36c7cf474c0ec63
alderabane <pal3stine4ever@hotmail.com>
2013-03-24 21:38:42 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+  public static void main(String[] args)
+	{
+		Linear c1 =new Linear(); // there is another error !!! 
+		c1.inputdata();// call method input data this to get values
+		c1.solution();// call method solution this to solve equ
+		c1.printOut();// call method printout this to show result
+		
+		// now Dr dimitry told us to make object with values like this
+		Linear c2=new Linear(1,2); 
+		c2.solution();
+		c2.printOut();
+		// look at this we didnt use method input data becuase we entered the value here Linear(1,2)
+	}
+
+}
+// now everything ok lets try the program !!
+// ok :D but wait second where is the other part XD
+// lets try it 
+// every thing ok thnx for wathing and hope every one leraned something :D
+// salam 3lykom :)
